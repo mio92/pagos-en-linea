4cd7efec2d7c604d935abfbf22a2e1a28a4c78a9
mio92 <isabelortizcabrera@hotmail.com>
2025-09-08 20:33:42 -0500

    Prueba 32

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -43,7 +43,7 @@ const TransactionList: React.FC<TransactionListProps> = ({ transactions }) => {
             <div className="transaction-amount">
               {formatCurrency(transaction.amount)}
               <p>crea en el dia de hoy: {formatDate(transaction.created_at)}</p>
-              <p>crea en el dia de hoy: {formatDate(transaction.created_at)}</p>
+              {formatDate(transaction.created_at)}
             </div>
           </div>
         ))}
