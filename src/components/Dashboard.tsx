fcb920910b6dbbafc3cf97054024ba81b030a2a2
mio92 <isabelortizcabrera@hotmail.com>
2025-09-11 17:55:22 -0500

    prueba 60

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -71,8 +71,6 @@ const Dashboard: React.FC = () => {
         <p>Este es un ajuste no tan grande</p>
         <p>Este es un ajuste no tan grande</p>
         <h3>Total Transactions</h3>
-        <p>Este es un ajuste no tan grande</p>
-        <p>Este es un ajuste no tan grande</p>
             <h3>Active Users</h3>
             <p className="stat-number">{transactionSummary.totalTransactions}</p>
           </div>
