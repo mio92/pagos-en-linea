59d0b34aaac7b2b48c37bdce8d4456528db7dd73
mio92 <isabelortizcabrera@hotmail.com>
2025-09-23 14:13:31 -0500

    Ajusta tags preuba 14

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -75,8 +75,6 @@ const Dashboard: React.FC = () => {
         <p>Este es un ajuste no tan grande</p>
         <p>Este es un ajuste no tan grande</p>
             <h3>Total Transactions</h3>
-        <p>Este es un ajuste no tan grande</p>
-        <p>Este es un ajuste no tan grande</p>
             <p className="stat-number">{transactionSummary.totalTransactions}</p>
           </div>
         </div>
